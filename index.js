6800e326a5630ec862c6e4163d86a6d45e758033
Caleb Chang <changc42@gmail.com>
2020-01-22 00:30:13 -0500

    refactored index.js and separated keys.js into dev and prod. finished authflow

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const keys = require("./config/keys");
-require("./models/user");
-require("./services/passport.js");
-
 const app = express();
+require("./routes/middleware")(app);
+require("./routes/authRoutes")(app);
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 mongoose.connect(keys.mongoURI);
 
-require("./routes/authRoutes")(app);
+require("./models/user");
+
+require("./services/passport");
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log("running on port " + PORT));
