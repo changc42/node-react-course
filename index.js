6c3c15139d377c470fd789c3758980d3d62cef8b
Caleb Chang <changc42@gmail.com>
2020-01-22 23:47:33 -0500

    CRA client, http-proxy-middleware for 3000->3500, npm concurrently, async ES2016 syntax
    cra client, lh:3000/ displays hi there!
    used http-proxy-middleware to redirect requests to "/api" and "/auth/google"
      to 3500 (default is 3000)
    enabled "concurrently" startup
    converted async func in passport to ES2016 syntax

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const app = express();
-require("./routes/middleware")(app);
-require("./routes/authRoutes")(app);
 
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 mongoose.connect(keys.mongoURI);
 require("./models/user");
 
-const User = mongoose.model("users");
-app.get("/modify_database", (req, res) => {
-  new User({
-    googleId: "23453563747"
-  })
-    .save()
-    .then(res.send(process.env.MONGO_URI));
-});
-
-require("./models/user");
+require("./routes/middleware")(app);
+require("./routes/authRoutes")(app);
 
 require("./services/passport");
 
