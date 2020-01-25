3e812930945354e76e54b127f0440726bc96503d
Caleb Chang <changc42@gmail.com>
2020-01-24 23:00:32 -0500

    completed course up to video 85

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
 
-export default function App() {
-  return <div>Hi there!</div>;
+function Landing() {
+  return <h1>landing</h1>;
+}
+
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
+
+class App extends React.Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
