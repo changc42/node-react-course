6376157c6f0d84a3039e6a2319901729f10a4d98
Caleb Chang <changc42@gmail.com>
2020-01-26 22:10:48 -0500

    removed unnecessary mongo testing/debugging block

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ require("./routes/authRoutes")(app);
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 mongoose.connect(keys.mongoURI);
-require("./models/user");
-
-const User = mongoose.model("users");
-app.get("/modify_database", (req, res) => {
-  new User({
-    googleId: "23453563747"
-  })
-    .save()
-    .then(res.send(process.env.MONGO_URI));
-});
 
 require("./models/user");
 
@@ -23,5 +13,3 @@ require("./services/passport");
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log("running on port " + PORT));
-
-//dummy change
