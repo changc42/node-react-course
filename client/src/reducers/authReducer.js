6e8cebcbd945a14c0258c0ee23a9ad24fe82a1c9
Caleb Chang <changc42@gmail.com>
2020-01-27 01:59:42 -0500

    used ES2016 async syntax on src/actions/index.js

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
+import { FETCH_USER } from "../actions/types";
+
 export default (state = null, action) => {
   console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+
     default:
       return state;
   }
