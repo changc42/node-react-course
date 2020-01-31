f4574c3bb64aaa54f8c3145e6ab2aec8b4f65137
Caleb Chang <changc42@gmail.com>
2020-01-30 22:59:19 -0500

    configured express to find unrecognized routes in build files

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ require("./routes/billingRoutes")(app);
 
 require("./services/passport");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log("running on port " + PORT));
