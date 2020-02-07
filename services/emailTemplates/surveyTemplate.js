e6208babf5ed3cef8c46094b0bbc7955e17cc620
Caleb Chang <changc42@gmail.com>
2020-02-07 16:37:31 -0500

    bug. mail will not send

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,10 +1,23 @@
 module.exports = question => {
+  // return `
+  // <html>
+  //   <body>
+  //     <div style="text-align: center">
+  //       <h1>Help us improve!</h1>
+  //       <p>${question}</p>
+  //       <a href="https://localhost:3000">Yes</a>
+  //       <a href="https://localhost:3000">No</a>
+  //     </div>
+  //   </body>
+  // </html>
+
+  // `;
   return `
     <div style="text-align: center">
       <h1>Help us improve!</h1>
       <p>${question}</p>
-      <a>Yes</a>
-      <a>No</a>
+      <a href="https://localhost:3000">Yes</a>
+      <a href="https://localhost:3000">No</a>
     </div>
   `;
 };
