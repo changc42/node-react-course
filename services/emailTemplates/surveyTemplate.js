3e5a88be10e18fcff2a08c9b849bfe7dc58516e0
Caleb Chang <changc42@gmail.com>
2020-02-07 23:41:19 -0500

    subtract credits for survey creation, yes/no links redirects to TY page, save survey in mongo

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,10 +1,12 @@
+const keys = require("../../config/keys");
+
 module.exports = question => {
   return `
     <div style="text-align: center">
       <h1>Help us improve!</h1>
       <p>${question}</p>
-      <a>Yes</a>
-      <a>No</a>
+      <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+      <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
     </div>
   `;
 };
