641bc047a88752dc84c6bab2de282f5d35d93394
Caleb Chang <changc42@gmail.com>
2020-02-08 00:54:12 -0500

    added survey field/form components

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
 
 export default class SurveyNew extends Component {
   render() {
     return (
       <div>
-        <p>SurveyNew</p>
+        <SurveyForm />
       </div>
     );
   }
