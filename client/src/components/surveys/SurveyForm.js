7fce79b5c6be9056ae8002f9dd7ac0456c86bcb2
Caleb Chang <changc42@gmail.com>
2020-02-08 23:17:55 -0500

    shortened duplicate form components and added styling and cancel button

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,35 +1,28 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+
+const fields = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
+];
 
 class SurveyForm extends Component {
   renderFields() {
     return (
       <div>
-        <Field
-          label="Survey Title"
-          type="Text"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          label="Subject Line"
-          type="Text"
-          name="subject"
-          component={SurveyField}
-        />
-        <Field
-          label="Email Body"
-          type="Text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="Text"
-          name="emails"
-          component={SurveyField}
-        />
+        {fields.map(e => (
+          <Field
+            key={e.name}
+            label={e.label}
+            type="text"
+            name={e.name}
+            component={SurveyField}
+          />
+        ))}
       </div>
     );
   }
@@ -38,7 +31,13 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
